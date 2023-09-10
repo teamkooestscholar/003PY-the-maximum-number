ebfa26233c2b21ec62d2ed46a4b24ad39dd449bd
Jonash Gania <jonashtiglao@gmail.com>
2023-09-10 15:53:44 +0800

    return largest and smallest number

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,14 @@
-# Write a function that returns the largest element in a list.
-def find_maximum(_):
-    pass
+# function to return the largest and smallest number is list
+
+def find_max_number(numbers):
+    if len(numbers) == 0:
+        print("list is empty")
+    else:
+        max_num = max(numbers)
+        min_num = min(numbers)
+        
+        print(f"Max number: {max_num}, Min number: {min_num}")
+    
+list = [3, 4, 0, -1, 30, 44, 23, -5]
+
+find_max_number(list)
