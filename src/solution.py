35f02a25e645c3a6e6ec07794a1419b0b67e2bd9
Jayem0709 <123857306+Jayem0709@users.noreply.github.com>
2023-09-11 00:21:40 +0800

    solution for find min and max

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,19 @@
-# Write a function that returns the largest element in a list.
-def find_maximum(_):
-    pass
+# # Write a function that returns the largest element in a list.
+# def find_maximum(_):
+#     pass
+
+
+def find_max_and_min(numbers):
+    if not numbers:
+        return "Your list is empty, cannot find maximum or minimum."
+
+    maximum = max(numbers)
+    minimum = min(numbers)
+
+    return maximum, minimum
+
+# Example usage:
+numbers_list = [5, 2, 9, 1, 7, 4, 8]
+result = find_max_and_min(numbers_list)
+print("Maximum:", result[0])
+print("Minimum:", result[1])
