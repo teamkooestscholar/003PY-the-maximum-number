01a1b6693416bd22391d45586da850266d1b18d7
deguzmankaira <123879555+deguzmankaira@users.noreply.github.com>
2023-09-10 23:03:50 +0800

    003PY - Maximum Number

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,32 @@
 # Write a function that returns the largest element in a list.
-def find_maximum(_):
-    pass
+def find_maximum_minimum(numbers):
+    if len(numbers) == 0:
+        return None, None
+
+    max_number = min_number = numbers[0]
+
+    for number in numbers:
+        if number > max_number:
+            max_number = number
+        elif number < min_number:
+            min_number = number
+
+    return max_number, min_number
+
+numbers = []
+for i in range(5):
+    while True:
+        try:
+            number = int(input(f"Enter Your Number {i+1}: "))
+            numbers.append(number)
+            break
+        except ValueError:
+            print("Invalid input, enter an integer number.")
+
+max_num, min_num = find_maximum_minimum(numbers)
+if max_num is not None and min_num is not None:
+    print(f"Maximum number: {max_num}")
+    print(f"Minimum number: {min_num}")
+else:
+    print("Oops! The list is empty.")
+
