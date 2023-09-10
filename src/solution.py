1e8aaeb7f4d5c44ebdfe5b5b9c068695243e1109
gemoftruesight <kcdsanchezz@gmail.com>
2023-09-10 21:25:26 +0800

    committed

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,30 @@
-# Write a function that returns the largest element in a list.
-def find_maximum(_):
-    pass
+def max_and_min(nums):
+    if not nums:
+        return "List is empty. No max and min."
+
+    max_num = max(nums)
+    min_num = min(nums)
+
+    return max_num, min_num
+
+# Example usage:
+nums1 = [5, 10, 2, 8, 15,19,59,69]
+nums2 = []
+
+result1 = max_and_min(nums1)
+result2 = max_and_min(nums2)
+
+if isinstance(result1, tuple):
+    max1, min1 = result1
+    print(f"Max in nums1: {max1}")
+    print(f"Min in nums1: {min1}")
+else:
+    print(result1)
+
+if isinstance(result2, tuple):
+    max2, min2 = result2
+    print(f"Max in nums2: {max2}")
+    print(f"Min in nums2: {min2}")
+else:
+    print(result2)
+
