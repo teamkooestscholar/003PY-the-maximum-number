69773c2251099e23010aae6567127f4161fcd277
YoshiroSenpai <123640832+YoshiroTheDestroyer@users.noreply.github.com>
2023-09-11 00:40:23 +0800

    added a code

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,22 @@
-# Write a function that returns the largest element in a list.
-def find_maximum(_):
-    pass
+def find_max(numbers):
+    if not numbers:
+        return None  
+    else:
+        return max(numbers)
+
+user_input = input("Please Enter a list of numbers: ")
+input_values = user_input.split()  
+
+integer_list = []
+for value in input_values:
+    try:
+        integer_list.append(int(value))
+    except ValueError:
+        pass  
+
+maximum = find_max(integer_list)
+
+if maximum is not None:
+    print(f"The biggest number in this list is: {maximum}")
+else:
+    print("The list is not valid")
