3dbd4723a625fc80007f52eaba655bd201183281
Prgrmmr633 <airesmacalino09@gmail.com>
2023-09-11 08:44:58 +0800

    update

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,21 @@
-# Write a function that returns the largest element in a list.
-def find_maximum(_):
-    pass
+def find_maximum(numbers):
+    if len(numbers) == 0:
+        return None, None
+    max_number = numbers[0]
+    min_number = numbers[0]
+    
+    for number in numbers:
+        if number > max_number:
+            max_number = number
+        if number < min_number:
+            min_number = number
+    
+    return max_number, min_number
+
+numbers1 = [3, 6, 2, 8, 1]
+max1, min1 = find_maximum(numbers1)
+print(f"Maximum: {max1}, Minimum: {min1}")
+
+numbers2 = []
+max2, min2 = find_maximum(numbers2)
+print(f"Maximum: {max2}, Minimum: {min2}")
