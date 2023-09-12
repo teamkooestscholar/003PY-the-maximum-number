fd9a0bc081605924fa3518653634e42a1c333bfc
tobywan1 <shantoby00@yahoo.com>
2023-09-12 22:43:04 +0800

    Update solution.py

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,15 @@
 # Write a function that returns the largest element in a list.
 def find_maximum(_):
     pass
+def find_maximum(numbers):
+    if not numbers:
+       
+        return None
+    maximum = max(numbers)
+
+    return maximum
+
+
+number_list = [4, 7, 2, 9, 1, 11, 5]
+maximum_number = find_maximum(number_list)
+print("The maximum number is:", maximum_number)  
