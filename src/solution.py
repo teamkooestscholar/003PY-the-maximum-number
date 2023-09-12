45c09e847c045a37e03f47e194eabfb45bf274b9
Cy12swe <104562626+Cy12swe@users.noreply.github.com>
2023-09-12 22:38:24 +0800

    Finds the maximum and minimum number.

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,13 @@
-# Write a function that returns the largest element in a list.
-def find_maximum(_):
-    pass
+def find_maximum(numbers):
+    if not numbers:  # Check if the list is empty
+        return "The list is empty, there is no maximum or minimum value."
+
+    maximum = max(numbers)
+    minimum = min(numbers)
+    return maximum, minimum
+
+
+numbers = [5, 10, 15, 20, 25]
+max_num, min_num = find_maximum(numbers)
+print("The maximum number is:", max_num)
+print("The minimum number is:", min_num)
