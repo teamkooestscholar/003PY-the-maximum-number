df7ca60b93aca30b58212c6167fb9cbf47d9bbda
ChristianPaulo24 <123344269+ChristianPaulo24@users.noreply.github.com>
2023-09-12 22:38:00 +0800

    solution.py

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,21 @@
-# Write a function that returns the largest element in a list.
-def find_maximum(_):
-    pass
+def find_maximum(numbers):
+    if not numbers:
+        return "List is empty."
+    
+    maximum = numbers[0]  # Initialize maximum with the first element
+    
+    for number in numbers:
+        if number > maximum:
+            maximum = number
+    
+    return maximum
+
+# Input a list of numbers from the user
+try:
+    num_list = input("Enter a list of numbers separated by spaces: ").split()
+    num_list = [float(num) for num in num_list]  # Convert input to a list of floats
+except ValueError:
+    print("Invalid input. Please enter a list of numbers separated by spaces.")
+else:
+    maximum = find_maximum(num_list)
+    print(f"The maximum number in the list is: {maximum}")
